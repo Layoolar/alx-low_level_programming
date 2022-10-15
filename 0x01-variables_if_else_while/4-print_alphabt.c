f1f0cdc75d0c84953fa46664dc086d99a54a56c9
Olayiwola Ayoola <olaiwolaayoola@gmail.com>
2022-10-14 21:38:30 -0700

    All alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, then in uppercase.
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char lower = 'a';
+
+	while (lower <= 'z')
+	{
+	if (lower != 'q' || 'e')
+	{
+		putchar(lower);
+		lower++;
+	}
+	}
+	putchar('\n');
+	return (0);
+}
