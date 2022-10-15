d8bc20692be97c8573e18ca93f7c48d1279e2c07
Olayiwola Ayoola <olaiwolaayoola@gmail.com>
2022-10-14 18:28:39 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * entry point
- * Returns 0 Success
+ * main -entry point
+ * Return: 0 Success
 */
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
