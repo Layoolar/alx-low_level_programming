4a9442be161d4c121a4e7acf7c19ba709729e73f
Olayiwola Ayoola <olaiwolaayoola@gmail.com>
2022-10-14 21:49:03 -0700

    All alphabets

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, then in uppercase.
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char i =0
+
+	while(i < 10)
+	{
+	putchar(i);
+	i++;
+	}
+	return (0);
+}
