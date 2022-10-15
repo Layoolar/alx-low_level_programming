28987014b453c4bc1a8db33e78b964eaff4a4732
Olayiwola Ayoola <olaiwolaayoola@gmail.com>
2022-10-14 19:11:34 -0700

    Added helloffile

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,21 @@ int main (void)
 	long e;
 	long long f;
 
+	/**
 	printf("Size of an integer: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a double: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(e));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(f));
+	*/
+	
+	
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+		printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a);
+		printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+
+		printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
