caccb1e851ca65c5476dc6fc89b79548ad6bbe33
Olayiwola Ayoola <olaiwolaayoola@gmail.com>
2022-10-14 21:26:07 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,16 +9,12 @@ int main(void)
 {
 	char lower = 'a';
 	char upper = 'A';
-	
+
 	while (lower <= 'z')
 	{
 		putchar(lower);
 		lower++;
 	}
-	while (upper <= 'Z')
-	{
-		putchar(upper);
-		upper++;
-	}
+	putchar(\n);
 	return (0);
 }
