4a572478371dd7e9e289aa73a61f659f70c42219
Olayiwola Ayoola <olaiwolaayoola@gmail.com>
2022-10-14 18:41:32 -0700

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  * Main - Entry point
- * Return: 0 
+ * Return: 0
  *
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,");
-	return 0;
+	return (0);
 }
