0ef7dc85f0f5d5af60e39a8e39813e62da18e41d
Olayiwola Ayoola <olaiwolaayoola@gmail.com>
2022-10-14 20:14:19 -0700

    If else

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+
+/** main - Entry point 
+ *  return: 0 always true
+ */
+
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0) {
+	printf("%lu is positive\n", n);
+	}
+	else if (n == 0) {
+		printf("%lu is zero\n", n);
+	}
+	else {
+	printf("%lu is negative\n", n)
+	}
+	return (0);
+}
