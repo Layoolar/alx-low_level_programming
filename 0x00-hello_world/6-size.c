8c7b2aef1d895565ae98f10b0f97065657810093
Olayiwola Ayoola <olaiwolaayoola@gmail.com>
2022-10-14 18:57:31 -0700

    Sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main entry point
+ * return 0
+ */
+int main (void)
+{
+	int a
+	double b
+	char d
+	float c
+	print("Size of an integer: %lu byte(s)\n", (unsigned long)sizeof(a);
+	print("Size of a double: %lu byte(s)\n", (unsigned long)sizeof(b);
+	print("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d);
+	print("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(c);
+	return (0);
+}
