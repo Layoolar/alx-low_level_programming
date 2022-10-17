44d7ffbfb9e70711c1e82b25e20dc3c23127c2d5
Olayiwola Ayoola <olaiwolaayoola@gmail.com>
2022-10-17 13:00:47 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, then in uppercase.
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar(num);
+
+	putchar('\n');
+
+	return (0);
+}
