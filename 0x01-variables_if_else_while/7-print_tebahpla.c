e34dac83d64970591be4a7e360f61ad13d23e131
Olayiwola Ayoola <olaiwolaayoola@gmail.com>
2022-10-17 13:19:38 -0700

    Print reverse Alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, then in uppercase.
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char lower = 'z';
+
+	while (lower >= 'z')
+	{
+		putchar(lower);
+		lower--;
+	}
+	putchar('\n');
+	return (0);
+}
