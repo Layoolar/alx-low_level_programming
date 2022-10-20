3e0f4c129b193ce19873f2a52bba544a9b617f89
Olayiwola Ayoola <olaiwolaayoola@gmail.com>
2022-10-19 17:37:49 -0700

    Check alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - checks for alphabets
+ * Return: 0 or 1
+ * @c: input character
+ */
+
+int _islower(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
