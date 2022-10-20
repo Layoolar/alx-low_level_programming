c1588c4cfc7548cbf6e1dd2fcc72c4350aa4d5a1
Olayiwola Ayoola <olaiwolaayoola@gmail.com>
2022-10-19 17:17:50 -0700

    Print alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ * Description: Prints the alphabet with _putchar
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+char i;
+char j;
+
+for (j = 0; j < 10; j++)
+{
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	}
+}
