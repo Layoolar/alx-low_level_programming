8be292b32a9d9f66bc106b936de95e9996fab3ac
Olayiwola Ayoola <olaiwolaayoola@gmail.com>
2022-10-19 17:30:48 -0700

    Check lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - checks for lowercase letters
+ * Return: 0 or 1
+ */
+
+int _islower(int i)
+{
+	if (i >= 'a' && i <= 'z')
+		return (1);
+	else
+		return (0);
+}
