860eb9a5f724c944a8fbcc2163145df53ff53fc7
Layoolar <46034532+Layoolar@users.noreply.github.com>
2022-10-22 02:31:43 +0100

    Update 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0123456789\n
+ *
+ * Return: void
+ */
+
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
